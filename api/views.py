84a60aee24e02cd906ea79d800f0c7fe1bcb07d0
Vazgen <vouqreel@gmail.com>
2023-07-27 08:40:47 +0500

    getting serializer data method

--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from base.models import Item
+from .serializer import ItemSerializer
 
 @api_view(['GET'])
 def getData(request):
-   person = {'name': 'Vazgen', 'age': 19}
-   return Response(person) 
+   items = Item.objects.all()
+   serializer = ItemSerializer(items, many=True)
+   return Response(serializer.data) 
