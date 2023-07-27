ac4d373ed7c351071bd87b9513b657e0e34db68f
Vazgen <vouqreel@gmail.com>
2023-07-27 08:36:21 +0500

    adding new files and fixing mistakes

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framerwork'
+
+    'rest_framework',
+    'base'
 ]
 
 MIDDLEWARE = [
